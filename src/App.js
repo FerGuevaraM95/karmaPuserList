ac5b3d8543eec40c8602ddd6ded02dfcd91591e8
Fernando <luisfernandoguevara95@gmail.com>
2017-11-23 11:26:08 -0600

    Comit inicial

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      results: []
+    };
+
+    axios.get("https://randomuser.me/api/?results=50")
+    .then(response => {
+      this.setState({
+        results: response.data.results
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Hola Mundo</h1>
+        <ul>
+          {this.state.results.map(result =>
+            <li key={result.email}>{result.email} {result.login.username}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
